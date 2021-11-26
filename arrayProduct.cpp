511e6a538d55b981ca7bf9171b8977db2c254333
Brenda Kiptim <93176526+brendakiptim@users.noreply.github.com>
2021-11-26 16:58:58 +0300

    Update arrayProduct.cpp

--- arrayProduct.cpp
+++ arrayProduct.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main()
+
 {
     //declare and initialize variables
-    int ten[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int product = 1;
-
-    int size = *(&ten + 1) - ten;
+    double ten[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    double product = 1;
 
     //loop and calculate the sum
-    for (int x = 0; x < 10;)
+    for (int x = 0; x < 10; x++)
     {
         product = product * ten[x];
+        cout << product << "\n";
     };
     cout << "Total product is " << product << "  \n";
     return 0;
